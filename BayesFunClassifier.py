5002198f90c9186dd37cde26ebb6db189dc2882f
Marcel Ochocki <99407020+Makis965@users.noreply.github.com>
2023-05-08 20:06:00 +0200

    Update BayesFunClassifier.py

--- BayesFunClassifier.py
+++ BayesFunClassifier.py
@@ -5,46 +5,46 @@ import matplotlib.pyplot as plt
 import statistics as stats
 
 mi_1 = [0, 0]
-sigma_1 = [[2, -1],[-1,2]]
+sigma_1 = [[2, -1], [-1, 2]]
 
 mi_2 = [2, 2]
-sigma_2 = [[1,0],[0,1]]
+sigma_2 = [[1, 0], [0, 1]]
 
-data_C0 = np.random.multivariate_normal(mi_1,sigma_1,size =100)
+data_C0 = np.random.multivariate_normal(mi_1, sigma_1, size =100)
 class_C0 = np.zeros(100).transpose()
-data_C0 = np.column_stack((data_C0,class_C0))
-data_C1 = np.random.multivariate_normal(mi_2,sigma_2,size =100)
+data_C0 = np.column_stack((data_C0, class_C0))
+data_C1 = np.random.multivariate_normal(mi_2, sigma_2, size =100)
 class_C1 = np.ones(100).transpose()
-data_C1 = np.column_stack((data_C1,class_C1))
-data = np.concatenate((data_C0,data_C1),axis=0)
+data_C1 = np.column_stack((data_C1, class_C1))
+data = np.concatenate((data_C0, data_C1), axis=0)
 np.random.shuffle(data)
-train,test = np.split(data,2)
-train = pd.DataFrame(train, columns = ["X1","X2","CLASS"])
-test = pd.DataFrame(test, columns = ["X1","X2","CLASS"])
+train, test = np.split(data,2)
+train = pd.DataFrame(train, columns=["X1", "X2", "CLASS"])
+test = pd.DataFrame(test, columns=["X1", "X2", "CLASS"])
 
-def apriori_propability(train):
-    propability_C0 = len(train[train["CLASS"] == 0]) / len(train)
-    propability_C1 = len(train[train["CLASS"] == 1]) / len(train)
-    return propability_C0, propability_C1
+# def apriori_propability(train):
+#     propability_C0 = len(train[train["CLASS"] == 0]) / len(train)
+#     propability_C1 = len(train[train["CLASS"] == 1]) / len(train)
+#     return propability_C0, propability_C1
 
 training_set = train
 test_set = test
 
-def multiple_df_rows(data_frame,num_of_cols):
-    new_column = np.ones((len(data_frame),1,))
+def multiple_df_rows(data_frame, num_of_cols):
+    new_column = np.ones((len(data_frame), 1,))
     new_column = pd.DataFrame(new_column)
-    for i in range(0,num_of_cols):
+    for i in range(0, num_of_cols):
         if i == 0:
-            new_column = data_frame.iloc[:,i]
+            new_column = data_frame.iloc[:, i]
         else:
-            new_column *= data_frame.iloc[:,i]
-    output_df = pd.concat([new_column,data_frame.iloc[:,data_frame.shape[1]-1]],axis = 1)
+            new_column *= data_frame.iloc[:, i]
+    output_df = pd.concat([new_column, data_frame.iloc[:, data_frame.shape[1]-1]], axis=1)
     return output_df
 
 def gaussian_distribution_Bayes(training_set, test_set, number_of_features = 2):
 
-    df_C0 = pd.DataFrame(np.nan, index=np.arange(0,len(test_set)).tolist(),
-                         columns = training_set.columns.to_list())
+    df_C0 = pd.DataFrame(np.nan, index=np.arange(0, len(test_set)).tolist(),
+                         columns=training_set.columns.to_list())
     df_C0["CLASS"] = 0
     df_C1 = pd.DataFrame(np.nan, index=np.arange(0, len(test_set)).tolist(),
                          columns=training_set.columns.to_list())
@@ -62,30 +62,25 @@ def gaussian_distribution_Bayes(training_set, test_set, number_of_features = 2):
 
             f_x = 1/(sd * sqrt(2*pi)) * np.exp((-1*(df_test - mean)**2)/(2*sd**2))
             if class_id == 0:
-                df_C0.iloc[:,feature_id] = f_x
+                df_C0.iloc[:, feature_id] = f_x
             else:
                 df_C1.iloc[:, feature_id] = f_x
 
             feature_id += 1
-    df_C0 = multiple_df_rows(df_C0,number_of_features)
-    df_C1 = multiple_df_rows(df_C1,number_of_features)
 
-    merged_df = pd.concat([df_C0,df_C1],axis = 1)
-    classified = pd.DataFrame(np.multiply([merged_df.iloc[:,0] > merged_df.iloc[:,2]], 1).T)
+    df_C0=multiple_df_rows(df_C0, number_of_features)
+    df_C1=multiple_df_rows(df_C1, number_of_features)
+
+    merged_df = pd.concat([df_C0, df_C1], axis=1)
+    classified = pd.DataFrame(np.multiply([merged_df.iloc[:, 0] < merged_df.iloc[:, 2]], 1).T)
 
     # classification
     return classified
 
-df_C0,df_C1 = gaussian_distribution_Bayes(training_set, test_set)
-
-# wyznaczamy parametry dla gaussa dla kazdego punktu treninowego dla zadanego sasiedztwa h
-# dla punktu testowego wyznaczamy sume prawdopodobienstwa wystapienia go w rozkładach wszystkich punktow sasiedztwa
-# mnozymy prawdopodobienstwa wszystkich zmiennych i wybieramy te klase dla ktorej iloczyn jest wiekszy
-
-def parzen_window_Bayes(training_set, test_set, h):
+def parzen_window_Bayes(training_set, test_set, h,number_of_features):
     # creating empty data frames for each class to fill with density propabilities
-    df_C0 = pd.DataFrame(np.nan, index=np.arange(0,len(test_set)).tolist(),
-                         columns = training_set.columns.to_list())
+    df_C0 = pd.DataFrame(np.nan, index=np.arange(0, len(test_set)).tolist(),
+                         columns=training_set.columns.to_list())
     df_C0["CLASS"] = 0
     df_C1 = pd.DataFrame(np.nan, index=np.arange(0, len(test_set)).tolist(),
                          columns=training_set.columns.to_list())
@@ -101,7 +96,6 @@ def parzen_window_Bayes(training_set, test_set, h):
             # searching for the h-area neighbours in training set for test point
             observation = 0
             for x_test in df_test:
-                # x_test = df_test[2]
                 min_x_test = x_test - h
                 max_x_test = x_test + h
                 x_train_subset = df_train.loc[(df_train >= min_x_test) & (df_train <= max_x_test)]
@@ -129,6 +123,84 @@ def parzen_window_Bayes(training_set, test_set, h):
     df_C1 = multiple_df_rows(df_C1,number_of_features)
 
     merged_df = pd.concat([df_C0,df_C1],axis = 1)
-    classified = pd.DataFrame(np.multiply([merged_df.iloc[:,0] > merged_df.iloc[:,2]], 1).T)
+    classified = pd.DataFrame(np.multiply([merged_df.iloc[:,0] < merged_df.iloc[:,2]], 1).T)
 
     return classified
+
+classified = gaussian_distribution_Bayes(training_set, test_set, 2)
+parzen_kernel_classifier = parzen_window_Bayes(training_set, test_set, 0.6,2)
+# confussion martices
+
+from sklearn import metrics
+
+confusion_matrix_bayes = metrics.confusion_matrix(test_set.iloc[:,2], classified)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_bayes, display_labels = [False, True])
+cm_display.plot()
+plt.show()
+
+confusion_matrix_parzen = metrics.confusion_matrix(test_set.iloc[:,2], parzen_kernel_classifier)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_parzen, display_labels = [False, True])
+cm_display.plot()
+plt.show()
+
+ACC_bayes = (confusion_matrix_bayes[0,0]+confusion_matrix_bayes[1,1])/sum(sum(confusion_matrix_parzen))
+ACC_parzen_kernel = (confusion_matrix_parzen[0,0]+confusion_matrix_parzen[1,1])/sum(sum(confusion_matrix_parzen))
+
+# DATA CLASSIFICATION
+
+samples = 200
+features = 8
+C0_nfeat = np.zeros((samples, features))
+C1_nfeat = np.zeros((samples, features))
+
+
+for i in range((np.shape(C0_nfeat)[1])):
+    # C0_nfeat[:,i] = np.random.exponential(scale=1/0.5, size=samples)
+    C0_nfeat[:, i] = np.random.uniform(low=-1, high=1, size=samples)
+for i in range((np.shape(C1_nfeat)[1])):
+    C1_nfeat[:, i] = np.random.uniform(low = -1, high=1, size=samples)
+
+C0_nfeat = np.append(C0_nfeat,np.zeros([200,1]), axis=1)
+C1_nfeat = np.append(C1_nfeat,np.ones([200,1]), axis=1)
+import scipy.io
+
+dir = r'C:\MARCEL\STUDIA\DATA SCIENCE\Sem I\Classifiers\Bayes lab'
+from scipy.io import loadmat
+import os
+files = []
+for file in os.listdir(dir):
+    if file.endswith(".mat"):
+        files.append(os.path.join(dir, file))
+
+mat = loadmat(files[0])
+data_train = pd.DataFrame(mat.get('dane_train'))
+classes_train = pd.DataFrame(mat.get('etykiety_train'))
+frames_train= [data_train,classes_train]
+microarray_train = pd.concat(frames_train).T
+
+data_test = pd.DataFrame(mat.get('dane_test'))
+classes_test = pd.DataFrame(mat.get('etykiety_test'))
+frames = [data_test,classes_test]
+microarray_test = pd.concat(frames).T
+
+# synthetic 8 features dataset
+
+data_8ft = np.concatenate((C0_nfeat, C1_nfeat), axis=0)
+np.random.shuffle(data_8ft)
+train_8ft, test_8ft = np.split(data_8ft,2)
+col_names = ["1","2","3","4","5","6","7","8","CLASS"]
+train_8ft = pd.DataFrame(train_8ft, columns=col_names)
+test_8ft = pd.DataFrame(test_8ft, columns=col_names)
+
+classified_8ft = gaussian_distribution_Bayes(train_8ft, test_8ft, 8)
+parzen_kernel_classifier_8ft = parzen_window_Bayes(train_8ft, test_8ft, 1,8)
+
+confusion_matrix_bayes_8ft = metrics.confusion_matrix(test_8ft.iloc[:,8], classified_8ft)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_bayes_8ft, display_labels = [False, True])
+cm_display.plot()
+plt.show()
+
+confusion_matrix_parzen_8ft = metrics.confusion_matrix(test_8ft.iloc[:,8], parzen_kernel_classifier_8ft)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_parzen_8ft, display_labels = [False, True])
+cm_display.plot()
+plt.show()
